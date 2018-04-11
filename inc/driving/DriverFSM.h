9e08d9a35d96135ba1db8f1da7fddc8cb251af24
Max Zimmermann <max.zimmermann@haw-hamburg.de>
2018-04-11 23:17:21 +0200

    Added basic members and getter for DriverFSM

--- inc/driving/DriverFSM.h
+++ inc/driving/DriverFSM.h
@@ -8,21 +8,41 @@
 #ifndef DRIVING_DRIVERFSM_H
 #define DRIVING_DRIVERFSM_H
 
-#include <driving/DriverBaseState.h>
-#include <driving/PDT1.h>
-#include <hal/Motor.h>
+#include "driving/DriverBaseState.h"
+#include "driving/PDT1.h"
+#include "hal/Motor.h"
+#include "position/PositionManager.h"
+#include "position/Position.h"
+#include "position/Angle.h"
+#include "serial/messages/drive_types.h"
+
+class DriverBaseState;
 
 class DriverFSM {
 private:
 	DriverBaseState* currentState;
-	Motor& motorLeft;
-	Motor& motorRight;
+	Motor& leftMotor;
+	Motor& rightMotor;
 	PDT1 positionControl;
 	PDT1 angleControl;
+	PositionManager& pm;
+	Position targetPosition;
+	Angle targetAngle;
+	DriveSpeed speed;
+	DriveDirection direction;
 
 public:
-	DriverFSM(Motor& motorLeft, Motor& motorRight);
+	DriverFSM(Motor& motorLeft, Motor& motorRight, PositionManager& pm);
 	void update();
+	Motor& getLeftMotor();
+	Motor& getRightMotor();
+	PDT1& getPositionController();
+	PDT1& getAngleController();
+	PositionManager& getPositionManager();
+	Position& getTargetPosition();
+	Angle& getTargetAngle();
+	DriveSpeed& getDriveSpeed();
+	DriveDirection& getDriveDirection();
 	virtual ~DriverFSM();
 };
 
--- src/driving/DriverFSM.cpp
+++ src/driving/DriverFSM.cpp
@@ -5,25 +5,70 @@
  *      Author: max
  */
 
-#include <driving/DriverFSM.h>
-#include <driving/DriverBaseState.h>
-#include <driving/DriverStates.h>
-#include <constants.h>
-#include <hal/Motor.h>
+#include "driving/DriverFSM.h"
+#include "driving/DriverBaseState.h"
+#include "driving/DriverStates.h"
+#include "constants.h"
+#include "hal/Motor.h"
+#include "position/PositionManager.h"
 
-DriverFSM::DriverFSM(Motor& motorLeft, Motor& motorRight) :
+DriverFSM::DriverFSM(Motor& motorLeft, Motor& motorRight, PositionManager& pm) :
 		currentState(nullptr),
-		motorLeft(motorLeft),
-		motorRight(motorRight),
+		leftMotor(motorLeft),
+		rightMotor(motorRight),
 		positionControl(CONTROLLER_SYSTEM_POSITION_GAIN,
 				CONTROLLER_SYSTEM_POSITION_DELAYTIME,
 				CONTROLLER_SAMPLING_TIME),
 		angleControl(
 				CONTROLLER_SYSTEM_ANGLE_GAIN,
-				CONTROLLER_SYSTEM_ANGLE_GAIN,
-				CONTROLLER_SAMPLING_TIME)
-		 {
+				CONTROLLER_SYSTEM_ANGLE_DELAYTIME,
+				CONTROLLER_SAMPLING_TIME),
+		pm(pm),
+		targetPosition(pm.getPosition()),
+		targetAngle(pm.getHeading()),
+		speed(DriveSpeed::SLOW),
+		direction(DriveDirection::FORWARD) {
 	currentState = new Idle(*this);
+}
+
+void DriverFSM::update() {
+	currentState->doAction();
+}
+
+Motor& DriverFSM::getLeftMotor() {
+	return leftMotor;
+}
+
+Motor& DriverFSM::getRightMotor() {
+	return rightMotor;
+}
+
+PDT1& DriverFSM::getPositionController() {
+	return positionControl;
+}
+
+PDT1& DriverFSM::getAngleController() {
+	return angleControl;
+}
+
+PositionManager& DriverFSM::getPositionManager() {
+	return pm;
+}
+
+Position& DriverFSM::getTargetPosition() {
+	return targetPosition;
+}
+
+Angle& DriverFSM::getTargetAngle() {
+	return targetAngle;
+}
+
+DriveSpeed& DriverFSM::getDriveSpeed() {
+	return speed;
+}
+
+DriveDirection& DriverFSM::getDriveDirection() {
+	return direction;
 }
 
 DriverFSM::~DriverFSM() {
