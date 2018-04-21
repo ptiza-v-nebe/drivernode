97ac4b9d0194bc277c36e64d10e857a973449124
Max Zimmermann <max.zimmermann@haw-hamburg.de>
2018-04-21 17:47:32 +0200

    Calibration

--- inc/position/calibration_parameters.h
+++ inc/position/calibration_parameters.h
@@ -26,9 +26,9 @@ static constexpr float UM_PER_TICK_R = 48.53662; // calibrated 27.03.18
 // Parameters for small robot
 // /////////////////////////
 #ifdef SMALL_ROBOT
-static constexpr float TRACK_WIDTH_UM = 300691.375; // TODO: calibrate
-static constexpr float UM_PER_TICK_L = 49.58842; // TODO: calibrate
-static constexpr float UM_PER_TICK_R = 48.53662; // TODO: calibrate
+static constexpr float TRACK_WIDTH_UM = 180466.78125; // calibrated 21.04.18
+static constexpr float UM_PER_TICK_L = 48.68075; // calibrated 21.04.18
+static constexpr float UM_PER_TICK_R = 49.02682; // calibrated 21.04.18
 #endif
 
 
