f40b77d0e9231e45429fbfd38e4e32feb32753fd
Moritz Höwer <Moritz.Hoewer@haw-hamburg.de>
2018-05-10 13:01:07 +0200

    Changed speed

--- src/hal/ShootingBLDC.cpp
+++ src/hal/ShootingBLDC.cpp
@@ -27,7 +27,7 @@ static constexpr float STOP_PERCENT = 0.05f;
 /**
  * duty cycle percentage for running the motor
  */
-static constexpr float ON_PERCENT = 0.058f;
+static constexpr float ON_PERCENT = 0.0575f;
 
 /**
  * Constructs a ShootingBLDC
